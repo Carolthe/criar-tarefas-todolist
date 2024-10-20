f9226c1a3b4f12ca05da64725db9a9608432a9c0
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-10-20 20:37:16 +0100

    criar-tarefa-todolist

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -2,17 +2,21 @@ import React, {useState} from 'react'
 import Logo from '../img/Logo.png'
 
 export default function Lista (){
-    const [tarefa, setTarefa] = useState ("")
-    const [button, setButton] = useState ("")
+    const [tarefa, setTarefa] = useState([])
+    const [button, setButton] = useState("")
 
-    function change (ev){
+    function changeInput (ev){
         setTarefa(ev.target.value)
     }
     function buttonClick (){
         setButton(tarefa)
         setTarefa("")
     }
-
+    function apagarTarefa(){
+        setButton("")
+    }
+    
+   
     return(
         <>
         <img src={Logo} />
@@ -20,17 +24,20 @@ export default function Lista (){
         <input className='w-52 bg-gray-800 rounded-lg text-white max-w-2xl h-[50px] sm:w-screen '
         type='text'
         value={tarefa}
-        onChange={change}
+        onChange={changeInput}
         />
         <button className='bg-blue-400 rounded-lg m-2 h-[50px] text-white'
-        onClick={buttonClick}
+       onClick={buttonClick}
  >Adicionar</button>
        </div> 
-       
+       { button &&
        <div className='flex justify-center'>
-      { button && <p className='bg-gray-500 w-[400px] h-[50px] '>{button} </p>
-        }  </div>
-      </>
+    <p className='bg-gray-500 w-[400px] h-[50px] justify-between flex p-3 '>{button} <button
+    onClick={apagarTarefa}
+    >X</button> </p>
+         </div>
+    }  
+    </>
      
     )
 }
