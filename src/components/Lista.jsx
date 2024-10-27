0e485410e5254a0a981caf4abb939f45bea0f8b5
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-10-27 19:24:18 +0000

    criar-tarefas-todolist

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -2,41 +2,37 @@ import React, {useState} from 'react'
 import Logo from '../img/Logo.png'
 
 export default function Lista (){
-    const [tarefa, setTarefa] = useState([])
-    const [button, setButton] = useState("")
+    const [tarefaInput, setTarefaInput] = useState([])
+    const [setarButton, setSetarButton] = useState("")
 
-    function changeInput (ev){
-        setTarefa(ev.target.value)
+    function clickButton (){
+        setSetarButton(tarefaInput)
+        setTarefaInput("")
     }
-    function buttonClick (){
-        setButton(tarefa)
-        setTarefa("")
-    }
-    function apagarTarefa(){
-        setButton("")
+    function apagarTarefa (){
+        setSetarButton("")
     }
     
-   
     return(
         <>
         <img src={Logo} />
        <div>
-        <input className='w-52 bg-gray-800 rounded-lg text-white max-w-2xl h-[50px] sm:w-screen '
+        <input className='w-52 bg-gray-800 rounded-lg text-white max-w-2xl h-[50px] sm:w-screen mt-14 '
         type='text'
-        value={tarefa}
-        onChange={changeInput}
+        value={tarefaInput}
+        onChange={(ev)=> setTarefaInput(ev.target.value)}
         />
-        <button className='bg-blue-400 rounded-lg m-2 h-[50px] text-white'
-       onClick={buttonClick}
- >Adicionar</button>
+        <button className='bg-blue-400 rounded-lg m-2 h-[50px] w-14 text-white'
+       onClick={clickButton}
+ >Add</button>
        </div> 
-       { button &&
+       {setarButton &&
        <div className='flex justify-center'>
-    <p className='bg-gray-500 w-[400px] h-[50px] justify-between flex p-3 '>{button} <button
+    <p className='bg-gray-500 w-[400px] h-[50px] justify-between flex p-3 '>{setarButton} <button
     onClick={apagarTarefa}
-    >X</button> </p>
+    >X</button></p>
          </div>
-    }  
+    }
     </>
      
     )
