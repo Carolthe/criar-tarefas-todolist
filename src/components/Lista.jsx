b2982361e07212ca14ef787cecc4d6b898ead7ad
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-11-05 19:22:47 +0000

    criar-tarefas-todolist

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -27,8 +27,9 @@ export default function Lista() {
       <img className='ml-36' src={Logo} alt="Logo" />
       <div>
         <input
-          className='focus:border-blue-800 w-52 ml-40 p-5 bg-gray-400 rounded-lg text-white max-w-2xl h-[50px] sm:w-screen mt-14'
+          className='w-52 ml-40 p-5 bg-gray-400 rounded-lg text-white max-w-2xl h-[47px] sm:w-screen mt-14'
           type='text'
+          disabled
           value={tarefaInput}
           onChange={(ev) => setTarefaInput(ev.target.value)}
           placeholder="Digite uma nova tarefa..."
@@ -44,14 +45,14 @@ export default function Lista() {
         <p className='text-white'>Tarefas Criadas <button className='w-4 bg-slate-300 rounded-full text-black'>{tarefas.length}</button></p>
         <p className='text-white'>Concluidas<button className='w-5 bg-slate-100 rounded-full text-black'>{teste.length}</button></p>
     </div>
-    <hr className='w-[720px] ml-[150px] mb-9'/>
+    <hr className='border-gray-400  w-[720px] ml-[150px] mb-9'/>
 
       {tarefas.map((task, index) => (
         <div className='flex justify-center' key={index}>
-          < p className='bg-gray-500 w-[650px] ml-[150px] h-[45px] rounded-lg justify-between flex p-3 mb-6'>
+          <p id="teste" className='bg-gray-500 w-[650px] ml-[150px] h-[45px] rounded-lg justify-between flex p-3 mb-6'>
           <button onClick={somar} className='w-5 bg-slate-100 rounded-full' ></button>
             {task}
-            <button onClick={() => apagarTarefa(index)}>X</button>
+            <button id='teste2' className='text-black' onClick={() => apagarTarefa(index)}>X</button>
           </p>
         </div>
       ))}
